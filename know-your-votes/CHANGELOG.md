b81a868e11e0a3e7db10b6532fa58036dab0f53a
phucmp <phucmp@Pauls-MacBook-Pro.local>
2020-09-03 16:47:28 -0700

    change states color

--- know-your-votes/CHANGELOG.md
+++ know-your-votes/CHANGELOG.md
@@ -14,6 +14,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Changed
 - Updated Main Home Section with responsiveness
+- Update css for svg map 
 
 ### Fixed
 
--- know-your-votes/src/components/MapDisplay.vue
+++ know-your-votes/src/components/MapDisplay.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <checkbox-svg-map v-model="selectedLocations" :map="customUSA" />
+        <checkbox-svg-map v-model="selectedLocations" :map="customUSA" @click="onClick" />
     </div>
 </template>
 
@@ -17,12 +17,44 @@ export default {
         return {
             customUSA: {
                 ...USA,
-                label: "US MAP"
+                label: "US MAP",
+                // locations: USA.locations.map(location => {
+                //   // Modify each location to customize/add attributes of <path>
+                // })
             },
             selectedLocations: []
         }
+    },
+    methods: {
+      onClick(event) {
+        console.log(event);
+      }
+    },
+    beforeUpdate: function() {
+      alert('hi');
     }
 }
 </script>
 
-<style src="vue-svg-map/dist/index.css"></style>
+<style>
+.svg-map {
+  width: 100%;
+  height: auto;
+  stroke: #fff;
+  stroke-width: 1;
+  stroke-linecap: round;
+  stroke-linejoin: round; 
+}
+.svg-map__location {
+  fill: #bdbec7;
+  cursor: pointer; 
+}
+.svg-map__location:focus, .svg-map__location:hover {
+  /* fill: #1494E9; */
+  fill: #ED3537;
+  outline: 0; 
+}
+.svg-map__location[aria-checked="true"] {
+  fill: #1494E9; 
+}
+</style>
